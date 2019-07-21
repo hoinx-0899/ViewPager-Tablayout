71ec3ec2a7b3ac0edcb55526e11274d50f94fa75
hoinx-0899 <nguyen.xuan.hoi@sun-asterisk.com>
2019-07-21 15:55:53 +0700

    test

--- app/src/main/java/com/sun/viewpager_tablayout/ThreeFragment.kt
+++ app/src/main/java/com/sun/viewpager_tablayout/ThreeFragment.kt
@@ -14,6 +14,7 @@ class ThreeFragment :Fragment(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("Three-FRAGMENT","onCreate")
+        Log.d("Three-FRAGMENT","onCreate")
 
     }
 
